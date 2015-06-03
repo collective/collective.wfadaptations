9c0ec141affb5169762d4e74facb8eee1deffea9
Cédric Messiant <cedricmessiant@ecreall.com>
2015-06-03 16:20:55 +0200

    Update update.sh script for i18n and French translations

--- src/collective/wfadaptations/locales/update.sh
+++ src/collective/wfadaptations/locales/update.sh
@@ -1,4 +1,5 @@
+domain=collective.wfadaptations
 i18ndude rebuild-pot --pot $domain.pot --create $domain --merge $domain-manual.pot ../
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
-domain=plone
-i18ndude sync --pot $domain-manual.pot */LC_MESSAGES/$domain.po
+#domain=plone
+#i18ndude sync --pot $domain-manual.pot */LC_MESSAGES/$domain.po
