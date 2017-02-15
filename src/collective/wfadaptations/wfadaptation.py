d1c4c12c873b618934a262219db73305b03e9cf3
Stephan Geulette <s.geulette@imio.be>
2017-02-15 21:21:48 +0100

    Removed z3c.jbot

--- src/collective/wfadaptations/wfadaptation.py
+++ src/collective/wfadaptations/wfadaptation.py
@@ -13,10 +13,19 @@ class WorkflowAdaptationBase(object):
     schema = None  # must be replaced with empty Schema
 
     def check_state_in_workflow(self, workflow, state_name):
-        """ Check if state_name is a state of workflow """
+        """ Check if state_name is a workflow state"""
         if state_name not in workflow.states:
             message = _("The workflow id '${id}' (${title}) doesn't contain the state '${state}'.",
                         mapping={'id': workflow.id, 'title': workflow.title, 'state': state_name})
+            return message
+        else:
+            return ''
+
+    def check_transition_in_workflow(self, workflow, transition_name):
+        """ Check if transition_name is a workflow transition """
+        if transition_name not in workflow.transitions:
+            message = _("The workflow id '${id}' (${title}) doesn't contain the transition '${transition}'.",
+                        mapping={'id': workflow.id, 'title': workflow.title, 'transition': transition_name})
             return message
         else:
             return ''
